9ed3884eaddb74acaf790d315822ebaa09e1238b
David Westerink <davidakachaos@gmail.com>
2016-10-17 16:01:41 +0200

    Easy on the queries
    We dont want to hit the db every request

--- lib/workless/middleware/workless_checker.rb
+++ lib/workless/middleware/workless_checker.rb
@@ -5,10 +5,24 @@ class WorklessChecker
 
   def call(env)
     status, headers, response = @app.call(env)
+    return [status, headers, response] if file?(headers) || empty?(response)
 
-    Delayed::Job.scaler.up if Delayed::Job.scaler.jobs.size > 0
-    
-    return [status, headers, response]
+    Delayed::Job.scaler.up unless Delayed::Job.scaler.jobs.empty?
+
+    [status, headers, response]
   end
 
+  # fix issue if response's body is a Proc
+  def empty?(response)
+    # response may be ["Not Found"], ["Move Permanently"], etc.
+    (response.is_a?(Array) && response.size <= 1) ||
+      !response.respond_to?(:body) ||
+      !response.body.respond_to?(:empty?) ||
+      response.body.empty?
+  end
+
+  # if send file?
+  def file?(headers)
+    headers['Content-Transfer-Encoding'] == 'binary'
+  end
 end
